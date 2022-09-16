e9b60e1221c00e8d8b189c41d36ff864ca0268f9
dinarabn2 <107791872+dinarabn2@users.noreply.github.com>
2022-09-16 16:08:30 +0600

    working with slides

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import modals from './modal';
+import sliders from './slides';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical')
 });
